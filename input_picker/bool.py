4734d69f4bfff6d95905e488ec18f5aa5af2c556
Cologler <skyoflw@gmail.com>
2017-12-21 12:24:42 +0800

    add bool style

--- input_picker/bool.py
+++ input_picker/bool.py
@@ -10,13 +10,18 @@ from .common import Option, ExceptionOption, Picker, Stop, Help
 
 
 def pick_bool(defval: bool=True,
+        use_bool_style: bool=False,
         raise_on_help: bool=True) -> bool:
     ''' pick a bool value. '''
     if not isinstance(defval, bool):
         raise TypeError('defval must be a bool value')
     options = Picker(sep='  ')
-    options.add(Option('Yes', ['Y', 'yes'], True))
-    options.add(Option('No', ['N', 'no'], False))
+    if use_bool_style:
+        options.add(Option('True', ['T', 'true'], True))
+        options.add(Option('False', ['F', 'false'], False))
+    else:
+        options.add(Option('Yes', ['Y', 'yes'], True))
+        options.add(Option('No', ['N', 'no'], False))
     options.add(ExceptionOption('Stop', ['S', 'stop'], Stop))
     if raise_on_help:
         options.add(ExceptionOption('Help', ['?', 'H'], Help))
