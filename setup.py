30dc2b4f189d2dc764c706d64a51903a521eeeb1
Cologler <skyoflw@gmail.com>
2018-01-17 03:03:02 +0800

    update to v0.1.2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=find_packages(),
     # auto generated:
     name='input_picker',
-    version='0.1.1.0',
+    version='0.1.2.0',
     description='',
     keywords=[],
     author='cologler',
